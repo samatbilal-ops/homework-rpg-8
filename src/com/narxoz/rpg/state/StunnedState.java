2761802d7dd460eae1f89e0b07ea52591f23ab4f
Bilal Samat <samat.bilal@narxoz.kz>
2026-04-25 01:06:00 +0500

    Add StunnedState

--- src/com/narxoz/rpg/state/StunnedState.java
+++ src/com/narxoz/rpg/state/StunnedState.java
@@ -1,4 +1,44 @@
 package com.narxoz.rpg.state;
 
-public class StunnedState {
+import com.narxoz.rpg.combatant.Hero;
+
+public class StunnedState implements HeroState {
+
+    private int turnsRemaining;
+
+    public StunnedState() {
+        this(1);
+    }
+
+    public StunnedState(int duration) {
+        this.turnsRemaining = duration;
+    }
+
+    @Override
+    public String getName() { return "Stunned"; }
+
+    @Override
+    public int modifyOutgoingDamage(int basePower) {
+        return 0;
+    }
+
+    @Override
+    public int modifyIncomingDamage(int rawDamage) { return rawDamage; }
+
+    @Override
+    public void onTurnStart(Hero hero) {
+        System.out.println("  [" + hero.getName() + "] is stunned and cannot move");
+    }
+
+    @Override
+    public void onTurnEnd(Hero hero) {
+        turnsRemaining--;
+        if (turnsRemaining <= 0 && hero.isAlive()) {
+            System.out.println("  [" + hero.getName() + "] shakes off the stun");
+            hero.setState(new NormalState());
+        }
+    }
+
+    @Override
+    public boolean canAct() { return false; }
 }
